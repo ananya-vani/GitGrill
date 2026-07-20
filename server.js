d1655408e5b7de5937eb3315221ad8ae2dd8e5f8
ananya-vani <ananya220406@gmail.com>
2026-07-20 19:38:13 +0530

    Configure Express for Vercel serverless

--- server.js
+++ server.js
@@ -76,9 +76,4 @@ app.post("/roast", async (req, res) => {
     });
   }
 });
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`GitGrill running on http://localhost:${PORT}`);
-});
+module.exports = app;
